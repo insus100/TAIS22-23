773107c9387fe636179d609365cecd735f0f5558
insus100 <37269595+insus100@users.noreply.github.com>
2022-09-23 12:20:31 +0200

    pajaros en vuelo finish

--- TAISProblems/Pajaros_en_vuelo/Pajaros_en_vuelo.cpp
+++ TAISProblems/Pajaros_en_vuelo/Pajaros_en_vuelo.cpp
@@ -25,42 +25,28 @@ using namespace std;
  // Escribe el código completo de tu solución aquí debajo
  // ================================================================
  //@ <answer>
-
-class ColaMedianas {
-public:
-    /*Monticulo de maximos con elementos menore que la mediana incluida*/
-    priority_queue<int> menores;
-
-    /*Monticulo de maximos*/
-    priority_queue<int, vector<int>, greater<int>> mayores;
-
-    void reequilibra() {
-        if (menores.size() < mayores.size()) {
-            menores.push(mayores.top());
-            mayores.pop();
-        }
-        else if (menores.size() > mayores.size() + 1) {
-            mayores.push(menores.top());
-            menores.pop();
-        }
+void reequilibra(priority_queue<int>& menores, priority_queue<int, vector<int>, greater<int>>& mayores) {
+    if (menores.size() < mayores.size()) {
+        menores.push(mayores.top());
+        mayores.pop();
     }
-
-    void insertar(int x) {
-        if (menores.empty())
+    else if (menores.size() > mayores.size() + 1) {
+        mayores.push(menores.top());
+        menores.pop();
+    }
+}
+void insertar(priority_queue<int>& menores, priority_queue<int, vector<int>, greater<int>>& mayores, int x) {
+    if (menores.empty())
+        menores.push(x);
+    else {
+        int med = menores.top();
+        if (x < med)
             menores.push(x);
-        else {
-            int med = menores.top();
-            if (x < med)
-                menores.push(x);
-            else menores.push(med);
-        }
+        else 
+            mayores.push(x);
+        reequilibra(menores, mayores);
     }
-
-    ColaMedianas() {
-
-    }
-};
-
+}
 bool resuelveCaso() {
 
     // leer los datos de la entrada
@@ -70,16 +56,20 @@ bool resuelveCaso() {
         return false;
 
     // resolver el caso posiblemente llamando a otras funciones
-    priority_queue<int> edades;
-    edades.push(E);//E es edad del primer pajaro
+   /*Monticulo de maximos con elementos menore que la mediana incluida*/
+    priority_queue<int> menores;
+    /*Monticulo de maximos*/
+    priority_queue<int, vector<int>, greater<int>> mayores;
+    insertar(menores, mayores, E);
     for (int i = 0; i < N; i++) {
         int p1, p2;
         cin >> p1 >> p2;
-        edades.push(p1);
-        edades.push(p2);
+        insertar(menores, mayores, p1);
+        insertar(menores, mayores, p2);
+        cout << menores.top() << " ";
     }
 
-    // escribir la solución
+    cout << "\n";
 
     return true;
 }
