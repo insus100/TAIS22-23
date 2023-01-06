0764b8dc919fcccc36856fbb95e346092a201475
insus100 <inigovelasco@gmail.com>
2023-01-06 20:16:01 +0100

    SERPIENTES

--- TAISProblems/Haciendo_trampas_en_Serpientes_y_Escaleras/Haciendo_trampas_en_Serpientes_y_Escaleras.cpp
+++ TAISProblems/Haciendo_trampas_en_Serpientes_y_Escaleras/Haciendo_trampas_en_Serpientes_y_Escaleras.cpp
@@ -9,6 +9,7 @@
 #include <fstream>
 #include <climits>
 #include <queue>
+#include <vector>
 using namespace std;
 
 #include "Digrafo.h"  // propios o los de las estructuras de datos de clase
@@ -27,23 +28,41 @@ using namespace std;
  // Escribe el código completo de tu solución aquí debajo
  // ================================================================
  //@ <answer>
-int bfs(int vector<int> const& v, int const &N, int const &K) {
-    int destino = N * N;
-    queue<int> q;
-    vector<int> distancia(destino + 1, INF);
-    q.push(1);
-    while (!q.empty()) {
-        int v = q.front(); q.pop();
-        for (int i = 1; i <= K && v + i <= destino; ++i) {
-            if (distancia[w] == INF) {
-                distancia[w] = distancia[v] + 1;
-                if (w == destino)
-                    return distancia[destino];
-                q.push(w);
+class Juego
+{
+public:
+    Juego(Digrafo const& g, int s) : visit(g.V(), false), dist(g.V())
+    {
+        bfs(g, s);
+    }
+    int distancia(int v) const { return dist[v]; }
+
+private:
+    std::vector<bool> visit; //visit[v] existe camino de s a v
+    std::vector<int> dist;   //ant[v] distancia de s a v mas corta
+
+    void bfs(Digrafo const& g, int s)
+    {
+        std::queue<int> cola;
+        visit[s] = true;
+        dist[s] = 0;
+        cola.push(s);
+        while (!cola.empty())
+        {
+            int v = cola.front();
+            cola.pop();
+            for (int w : g.ady(v))
+            {
+                if (!visit[w])
+                {
+                    dist[w] = dist[v] + 1;
+                    visit[w] = true;
+                    cola.push(w);
+                }
             }
         }
     }
-}
+};
 
 
 bool resuelveCaso() {
@@ -53,13 +72,37 @@ bool resuelveCaso() {
     cin >> N >> K >> S >> E;
     if (N == 0)
         return false;
-
-    vector<int>
+    int casillas = N * N;
+    vector<int> finAtajo(casillas + 1, -1);
+    Digrafo g(casillas + 1);
+    for (int i = 0; i < S + E; ++i)
+    { //serpientes y escaleras
+        int v, w;
+        std::cin >> v >> w;
+        finAtajo[v] = w;
+    }
+    for (int i = 1; i <= casillas; ++i)
+    { //paso de casilla en casilla
+        for (int j = 1; j <= K; j++)
+        {
+            int extremo = i + j;
+            if (extremo <= casillas)
+            {
+                if (finAtajo[extremo] != -1)
+                    g.ponArista(i, finAtajo[extremo]);
+                else
+                    g.ponArista(i, extremo);
+            }
+        }
+    }
 
     // resolver el caso posiblemente llamando a otras funciones
-
+    Juego Serpiente(g, 1);
     // escribir la solución
-
+    if (g.V() == 0)
+        std::cout << "1" << std::endl;
+    else
+        std::cout << Serpiente.distancia(casillas) << std::endl;
     return true;
 }
 
